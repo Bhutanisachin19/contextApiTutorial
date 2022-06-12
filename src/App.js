f63388ee377423c3171c9025862aec69c1485606
Bhutanisachin19 <sachin.bhutani60@gmail.com>
2022-06-12 18:45:20 +0530

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,56 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState, useReducer } from "react";
 import "./App.css";
 
-import HomePage from "./components/HomePage/HomePage";
-import FeedPage from "./components/FeedPage/FeedPage";
+const reducerFunction = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "decrement":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "initCount":
+      return {
+        ...state,
+        count: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-import AppState from "./context/AppState";
+const initialState = {
+  count: 0,
+};
 
 function App() {
+  const [input, setInput] = useState(0);
+  const [state, dispatch] = useReducer(reducerFunction, initialState);
+
   return (
     <div className="App">
-      <h1>Context Examples</h1>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/feed">Feed</Link>
-            </li>
-          </ul>
-        </nav>
-        <AppState>
-          <Switch>
-            <Route exact path="/home" component={HomePage} />
-            <Route exact path="/feed" component={FeedPage} />
-          </Switch>
-        </AppState>
-      </Router>
+      <h1>Reducer Example</h1>
+
+      <div>
+        <label>Start Count:</label>
+        <input
+          type="number"
+          onChange={(e) => setInput(parseInt(e.target.value))}
+          value={input}
+        />
+        <br />
+        <button onClick={() => dispatch({ type: "initCount", payload: input })}>
+          Initialize Counter
+        </button>
+      </div>
+
+      <p>{state.count}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
     </div>
   );
 }
