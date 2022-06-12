7f2456a0262ffbf2f0c9d7699287ff27cd201aba
Bhutanisachin19 <sachin.bhutani60@gmail.com>
2022-06-12 18:47:36 +0530

    Complete application using useReducer and useContext

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
-import React, { useState, useReducer } from "react";
+import React from "react";
 import "./App.css";
+import TodoInput from "./components/TodoInput/TodoInput";
+import TodoList from "./components/TodoList/TodoList";
 
-const reducerFunction = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return {
-        ...state,
-        count: state.count + 1,
-      };
-    case "decrement":
-      return {
-        ...state,
-        count: state.count - 1,
-      };
-    case "initCount":
-      return {
-        ...state,
-        count: action.payload,
-      };
-    default:
-      return state;
-  }
-};
-
-const initialState = {
-  count: 0,
-};
+import TodoState from "./context/TodoState";
 
 function App() {
-  const [input, setInput] = useState(0);
-  const [state, dispatch] = useReducer(reducerFunction, initialState);
-
   return (
-    <div className="App">
-      <h1>Reducer Example</h1>
-
-      <div>
-        <label>Start Count:</label>
-        <input
-          type="number"
-          onChange={(e) => setInput(parseInt(e.target.value))}
-          value={input}
-        />
-        <br />
-        <button onClick={() => dispatch({ type: "initCount", payload: input })}>
-          Initialize Counter
-        </button>
+    <div className="app">
+      <div className="app__todoApp">
+        <TodoState>
+          <TodoInput />
+          <TodoList />
+        </TodoState>
       </div>
-
-      <p>{state.count}</p>
-      <button onClick={() => dispatch({ type: "increment" })}>increment</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
     </div>
   );
 }
